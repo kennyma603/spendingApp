d186567fcf9eb65e99af9af0002c60131ba26c44
Kenny Ma <kma@central1.com>
2015-07-21 16:37:30 -0700

    added high charts

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('App', ['ionic'])
+angular.module('App', ['ionic', 'highcharts-ng'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 	$stateProvider.state('home', {
@@ -12,6 +12,7 @@ angular.module('App', ['ionic'])
 	});
 	$stateProvider.state('spendingSummary', {
 		url: '/spendingSummary',
+    controller: 'spendingSummaryController',
 		templateUrl: 'views/spendingSummary/spendingSummary.html'
 	});
 	$urlRouterProvider.otherwise('/home');
